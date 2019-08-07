5541e421dd7c771741c9136ca06a3d509acc34ed
homan <homan.kiani220@gmail.com>
2019-08-07 11:38:55 +0430

    add Bank model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Bank(models.Model):
+    branch              =   models.CharField(max_length=40,verbose_name="نام شعبه")
+    account_number      =   models.CharField(max_length=40,verbose_name="شماره حساب")
+    card_number         =   models.CharField(max_length=16,verbose_name="شماره کارت",null=True,blank=True)
+    shaba_number        =   models.CharField(max_length=26,verbose_name="شماره شبا",null=True,blank=True)
+    description         =   models.CharField(max_length=100,verbose_name="توضیحات",null=True,blank=True)
+
+    def __str__(self):
+        return "{}-{}".format(self.branch,self.account_number)
